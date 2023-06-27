bd6118168291e644c42b54e8fd94267cc208cbcc
Foyth-senai <124690153+Foyth-senai@users.noreply.github.com>
2023-06-27 12:25:41 -0300

    asda

--- sql/refigure.sql
+++ sql/refigure.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 27-Jun-2023 às 13:58
+-- Tempo de geração: 27-Jun-2023 às 17:23
 -- Versão do servidor: 10.4.28-MariaDB
 -- versão do PHP: 8.2.4
 
@@ -41,17 +41,18 @@ CREATE TABLE `cadastro` (
   `complementos` varchar(45) NOT NULL,
   `cidade` varchar(45) NOT NULL,
   `estado` varchar(45) NOT NULL,
-  `pais` text NOT NULL
+  `pais` text NOT NULL,
+  `bairro` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Extraindo dados da tabela `cadastro`
 --
 
-INSERT INTO `cadastro` (`id_cliente`, `nome_cliente`, `cpf_cliente`, `email_cliente`, `celular_cliente`, `senha_cliente`, `admin`, `logado`, `rua`, `numero_local`, `complementos`, `cidade`, `estado`, `pais`) VALUES
-(1, 'Jônatas Rocha dos Santos', '113.719.458-13', 'jonatasarcaro943@gmail.com', '47 98919-0438', '12341234', 1, 0, '', 0, '', '', '', ''),
-(2, 'Cauê Marchi Foyth', '125.413.329-12', 'foythcaue@gmail.com', '47 98823-0585', '12345678', 1, 0, 'Egon Tanner', 180, 'Casa', 'Joinville', 'SC', 'Brasil'),
-(3, 'Lucas Giovani Fruck', '071.524.669-04', 'lucas.f.giovani@gmail.com', '47 98908-0795', '87654321', 1, 0, '', 0, '', '', '', '');
+INSERT INTO `cadastro` (`id_cliente`, `nome_cliente`, `cpf_cliente`, `email_cliente`, `celular_cliente`, `senha_cliente`, `admin`, `logado`, `rua`, `numero_local`, `complementos`, `cidade`, `estado`, `pais`, `bairro`) VALUES
+(1, 'Jônatas Rocha dos Santos', '113.719.458-13', 'jonatasarcaro943@gmail.com', '47 98919-0438', '12341234', 1, 0, '', 0, '', '', '', '', ''),
+(2, 'Cauê Marchi Foyth', '125.413.329-12', 'foythcaue@gmail.com', '47 98823-0585', '12345678', 1, 1, 'Egon Tanner', 184, 'casa', 'Joinville', 'SC', 'Brasil', 'Petrópoils'),
+(3, 'Lucas Giovani Fruck', '071.524.669-04', 'lucas.f.giovani@gmail.com', '47 98908-0795', '87654321', 1, 0, '', 0, '', '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -73,9 +74,9 @@ CREATE TABLE `produtos` (
 --
 
 INSERT INTO `produtos` (`id_produto`, `nome_produto`, `preco_produto`, `qtd_produto`, `carrinho`, `preco_final`) VALUES
-(1, 'Urubu Preto e Branco', 70.00, 1, 1, 70.00),
+(1, 'Urubu Preto e Branco', 70.00, 1, 0, 70.00),
 (2, 'Burguês Preto e Branco', 60.00, 1, 0, 60.00),
-(3, 'Capitão Picanha', 130.00, 1, 1, 130.00),
+(3, 'Capitão Picanha', 130.00, 1, 0, 130.00),
 (4, 'Mago Cósmico', 150.00, 1, 0, 150.00),
 (5, 'Pirata Narigudo', 140.00, 1, 0, 140.00),
 (6, 'Robo Calcinha', 69.69, 1, 0, 69.69),
