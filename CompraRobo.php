cc949a6020834efa543a72b43ad5f8c12202a0db
Lucas Giovani Fruck <136374709+LucasGiovaniFruck@users.noreply.github.com>
2023-06-29 00:01:54 -0300

    Oi

--- CompraRobo.php
+++ CompraRobo.php
@@ -11,103 +11,146 @@ while ($linhas = $comando->fetch() )
      }
      include("conecta.php");
 ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>RE:Figure</title>
-    <link rel="stylesheet" type="text/css" href="css/Compra.css">
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap.css">
+    <title>Re:Figure</title>
     <link rel="icon" href="imagem/RE FIGURE.png">
+    <link rel="stylesheet" type="text/css" href="./css/tela_inicial3.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 </head>
 <body>
-    <script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
-    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-    <div class="cabecalho">
-        <a href="logado.php">
-        <img class="logo" src="imagem/RE FIGURE.png"></a>
-        <div class="container-input">
-            <input type="text" placeholder="Pesquisar" name="text" class="input">
-            <svg fill="#000000" width="20px" height="20px" viewBox="0 0 1920 1920" xmlns="http://www.w3.org/2000/svg">
-              <path d="M790.588 1468.235c-373.722 0-677.647-303.924-677.647-677.647 0-373.722 303.925-677.647 677.647-677.647 373.723 0 677.647 303.925 677.647 677.647 0 373.723-303.924 677.647-677.647 677.647Zm596.781-160.715c120.396-138.692 193.807-319.285 193.807-516.932C1581.176 354.748 1226.428 0 790.588 0S0 354.748 0 790.588s354.748 790.588 790.588 790.588c197.647 0 378.24-73.411 516.932-193.807l516.028 516.142 79.963-79.963-516.142-516.028Z" fill-rule="evenodd"></path>
-          </svg>
-          </div>
-          
-        <div class="canto">
-        <form action="logado.php" method="post">
-            <button name="botao" type="submit" class="btn">
-              <?php 
-                if($logado==1){
-                  $conta = "Conta";
-                  if(isset($_POST["botao"]) )
-                  {
-                    ?><script>window.location.replace("conta.php");</script><?php
-                  }
-                } else{
-                  $conta = "Entrar";
-                  if(isset($_POST["botao"]) )
-                  {
-                    ?><script>window.location.replace("login.php");</script><?php
-                  }
-                }
-              ?>
-                <p class="paragraph"> <?php echo($conta); ?> </p>
-                <span class="icon-wrapper">
-                    <ion-icon name="person-circle-outline"></ion-icon>
-                </span>
-              </button></form>
-              <?php 
-              ?>
-              <form action="logado.php" method="post">
-                <button class="btn" name="carrinho" type="submit">
-                <?php 
-                if($logado==1){
-                  if(isset($_POST["carrinho"]) )
-                  {
-                    ?><script>window.location.replace("carrinho.php");</script><?php
-                  }
-                } else{
-                  if(isset($_POST["carrinho"]) )
-                  {
-                    ?><script>window.location.replace("autenticacao.html");</script><?php
-                  }
-                } ?>
-                  <p class="paragraph"> Carrinho </p>
-                  <span class="icon-wrapper">
-                      <ion-icon name="cart-outline"></ion-icon>
-                  </span>
-                </button></form>
-        </div>
-    </div>
-    <div class="opcoes">
-        <nav class="opcoes_texto">
-          <ul>
-            <li><a href="lancamentos.html"> <b> Lançamentos</a> </b></li>
-            <li><a href="categorias.html"> <b> Categorias </a></b>
-              <ul>
-                <li><a href="categoria_animal.html"> <b> Animais</a></b></li>
-                <li><a href="categoria_pb.html"> <b> P&B</a></b></li>
-                <li><a href="categoria_colorido.html"> <b> Coloridos</a></b></li>
-              </ul>
+
+<i class="bi bi-list menu-mobile"></i>
+
+<aside id="aside"> <!--Menu lateral-->
+    <section class="profile"> <!--Seção da foto de perfil, nome, e social-links-->
+        <img src="./imagem/RE FIGURE.png" alt="Foto refigure"> <!--Foto pessoal-->
+        <h1>Re:Figure</h1>
+    </section>
+
+    <nav id="navbar" class="nav-menu"> <!--navbar lateral-->
+        <ul class="nav flex-column"> <!--Lista em column-->
+            <li class="nav-item">
+                <a class="nav-link" href="logado.php"><i class="bi bi-house"></i>Inicio</a>
             </li>
-            <li><a href="filamento.html"> <b> Filamento</a></b></li>
-            <li><a href="termos.html"> <b> Termos</a></b></li>
-            <li><a href="suporte.html"> <b> Suporte</a></b></li>
-          </ul>
-        </nav>
+            <li class="nav-item">
+                <a class="nav-link" href="conta.php"><i class="bi bi-person"></i>Conta</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="carrinho.php"><i class="bi bi-list-check"></i>Carrinho</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="categorias.html"><i class="bi bi-collection"></i>Categorias</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="filamento.html"><i class="bi bi-recycle"></i>Filamento</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="termos.html"><i class="bi bi-newspaper"></i>Termos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="suporte.html"><i class="bi bi-question-circle"></i>Suporte</a>
+            </li>
+        </ul>
+    </nav>
+</aside>
+    
+<section id="cabecalho" class="cabecalho alinhamento m-0 p-0">
+
+    <div class="cantologo col-md-4 d-flex justify-content-start align-itens-center text-align-center">
+
+        <img class="logo" id="logo" onclick="Logo1()" src="imagem/RE FIGURE.png">
+
     </div>
-    <div class="tudo">
-    <div class="esquerda">
-        <div class="outros">
-            <div class="outros1"> <img src="imagem/calcinha 2.png" width="100%"></div>
-            <div class="outros2"> <img src="imagem/calcinha 3.png" width="70%"></div>
-            <div class="outros3"> <img src="imagem/calcinha 4.png" width="100%"></div>
+    <div class="nome canto col-md-4">
+        <b>Re:Figure <i class="bi bi-tree"></i></b>
+    </div>  
+    <div class="canto col-md-4 justify-content-end align-itens-center text-align-center">
+
+            <button name="botao" class="btn me-2" onclick="Conta()">
+                <p class="paragraph mt-3"> Conta </p>
+                <span class="icon-wrapper">
+                    <i class="bi bi-person"></i>
+                </span>
+            </button>
+            <button class="btn me-3" name="carrinho" onclick="carrinho()">
+                <p class="paragraph mt-3"> Carrinho </p>
+                <span class="icon-wrapper">
+                    <i class="bi bi-cart2"></i>
+                </span>
+            </button>
+    </div>
+
+</section>
+
+<section id="opcoes" class="opcoes m-0 p-0">
+
+    <div class="alinhamento col-md-3">
+        <b onclick="lancamentos()" style="cursor: pointer;">Lançamentos</b>
+    </div>
+     
+    <div class="alinhamento col-md-2">
+        <b onclick="categorias()" style="cursor: pointer;">Categorias</b>
+    </div>
+     
+    <div class="alinhamento col-md-2">
+        <b onclick="filamento()" style="cursor: pointer;">Filamento</b>
+    </div>
+     
+    <div class="alinhamento col-md-2">
+        <b onclick="termos()" style="cursor: pointer;">Termos</b>
+    </div>
+     
+    <div class="alinhamento col-md-3">
+        <b onclick="suporte()" style="cursor: pointer;">Suporte</b>
+    </div>
+
+</section>
+
+<main id="main">
+
+<div id="produtinho class="container-fluid">
+    <div class="row z321">
+        <div class="col-md-7">
+    <section id="carrosel" class="m-0 p-0">
+    <div class="col-md-12">
+    <div class="slideshow-container mt-5 ms-4">
+
+        <div class="mySlides fade">
+          <img src="imagem/personagem6.png" style="width:100%; height:100%;">
         </div>
-        <div class="imagemprincipal"><img src="imagem/Robo Calcinha.png" width="100%"></div>
+        <div class="mySlides fade">
+          <img src="imagem/calcinha 2.png" style="width:100%; height:100%">
+        </div>
+        <div class="mySlides fade">
+          <img src="imagem/calcinha 4.png" style="width:100%; height:100%">
+        </div>
+      
+  
+        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+        <a class="next" onclick="plusSlides(1)">&#10095;</a>
+      </div>
+      <br>
+  
+      <div style="text-align:center">
+        <span class="dot" onclick="currentSlide(1)"></span>
+        <span class="dot" onclick="currentSlide(2)"></span>
+        <span class="dot" onclick="currentSlide(3)"></span>
+      </div>
+
     </div>
-    <div class="direita">
-        <div class="nome"><b>Robo Calcinha</b></div>
+
+
+    </section>
+        </div>
+        <div class="k321 col-md-5 mt-5">
+        <div class="nomee"><b>Robo Calcinha</b></div>
         <div class="descricao">
             <ul>
                 <li>Action Figure de um robo, olhos acendem. </li>
@@ -115,49 +158,64 @@ while ($linhas = $comando->fetch() )
                 <li>9cm de altura.</li>
             </ul>
         </div>
+        <form action="CompraRobo.php" method="post">
+        <input placeholder="Quantidade" value="1" name="quantidade" class="input-style" type="number" min="1" max="100">
         <div class="compr">
-            <div class="preco">
-                <div class="avista"><ion-icon name="cash-outline"></ion-icon><div class="formatarvalor"><h1>a partir de</h1><h2>R$69,69 </h2><h3>a vista 10% de desconto</h3></div></div>
-                <div class="parcelado"><ion-icon name="card-outline"></ion-icon><div class="formatarvalor"><h4>R$76,38</h4><h5>em até 6x de R$ 12.73 sem juros no cartão</h5></div></div>
+            <div class="precoo">
+                <div class="avista"><ion-icon name="cash-outline"></ion-icon><div class="formatarvalor"><h1>a partir de</h1><h2>R$69,69</h2><br><h3>a vista 10% de desconto</h3></div></div>
+                <div class="parcelado"><ion-icon name="card-outline"></ion-icon><div class="formatarvalor"><h4>R$76,38</h4><br><h5>em até 6x de R$ 12.73 sem juros no cartão</h5></div></div>
             </div>
         <div class="comp">
-        <form action="compraRobo.php" method="post"><button type="submit" class="comprar" name="comprar">Comprar</button></form>
+        <button type="submit" class="comprarr" name="comprar">Adicionar ao carrinho</button></FORM>
         <?php
-
             // Se clicou no botão comprar:
             if(isset($_POST["comprar"]) )
             {
-                $comando = $pdo->prepare("UPDATE produtos SET carrinho=1 WHERE id_produto = 6;");
+                $quantidade = $_POST["quantidade"];
+                $comando = $pdo->prepare("UPDATE produtos SET carrinho=1, qtd_produto='$quantidade' WHERE id_produto = 6;");
                 $resultado = $comando->execute();
                 ?><script>window.location.replace("carrinho.php");</script><?php
             }
             ?>
         </div>
         </div>
-    </div>
-    </div>
-    <div class="rodape">
-        <img class="logo_rodape" src="imagem/RE FIGURE.png">
-        <ul>
-          <a href="filamento.html"><li>Filamento</li></a>
-          <a href="lancamentos.html"><li>Lançamentos</li></a>
-          <a href="termos.html"><li>Termos</li></a>
-        </ul>
-        <ul>
-          <li>Desenvolvedores:</li>
-          <li>Cauê Marchi Foyth</li>
-          <li>Jônatas Rocha dos Santos</li>
-          <li>Lucas Giovani Fruck</li>
-        </ul>
-        <div class="contatos">
-          <ul>
-            <a href="https://instagram.com/caue_foyth" target="_blank"><li><ion-icon name="logo-instagram"></ion-icon>Cauê</li></a>
-            <a href="https://instagram.com/arcaro_jonatas" target="_blank"><li><ion-icon name="logo-instagram"></ion-icon>Jônatas</li></a>
-            <a href="https://instagram.com/lucasgiovanifruck" target="_blank"><li><ion-icon name="logo-instagram"></ion-icon>Lucas</li></a>
-            
-            
-          </ul>
         </div>
-        </div> 
+    <div>
+</div>
+
+<section id="rodape" class="m-0 p-0 mt-5">
+          <div class="alinharrow row">
+            <div class="alinhamentodalogo col-md-3">
+              <img class="logorodape" id="logo" onclick="Logo1()" src="imagem/RE FIGURE.png">
+            </div>
+            <div class="alinhamentorodape col-md-3">
+              <p>Copyright &copy; 2023 - All Rights Reserved - Re:Figure</p>
+            </div>
+            <div class="delete col-md-3">
+              <ul>
+                <li>Desenvolvedores:</li>
+                <li>Cauê Marchi Foyth</li>
+                <li>Jônatas Rocha dos Santos</li>
+                <li>Lucas Giovani Fruck</li>
+              </ul>
+            </div>
+            <div class="alinhamentorodape col-md-3">
+              <div class="divpai">
+                <ul class="insta">
+                  <a href="https://instagram.com/caue_foyth" target="_blank"><li><i class="bi bi-instagram"></i>Cauê</li></a>
+                  <a href="https://instagram.com/arcaro_jonatas" target="_blank"><li><i class="bi bi-instagram"></i>Jônatas</li></a>
+                  <a href="https://instagram.com/lucasgiovanifruck" target="_blank"><li><i class="bi bi-instagram"></i>Lucas</li></a>     
+                </ul>
+              </div>
+            </div>
+          </div>
+
+        </section>
+
+</main>
+
+
+    <script src="/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="./js/tela_inicial.js"></script>
 </body>
 </html>
